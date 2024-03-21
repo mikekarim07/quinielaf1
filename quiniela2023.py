192efa3795260e458a0e28b803e43e2b33496f22
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-03-20 20:16:27 -0600

    Update quiniela2023.py

--- quiniela2023.py
+++ quiniela2023.py
@@ -1,25 +1,36 @@
-# example/st_app.py
-
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+import os
+from supabase import create_client, Client
+import json
+from datetime import datetime
+
+url = 'https://uehrgoqjfbdbkkyumtpw.supabase.co'
+key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlaHJnb3FqZmJkYmtreXVtdHB3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwOTA3MDE1MywiZXhwIjoyMDI0NjQ2MTUzfQ.KIIsWOhJx7sPYYP6Wdvdq6S4vPJ8vrSrZbs-vG6kBWw'
+supabase_client = create_client(url, key)
+
+response_drivers = supabase_client.table('drivers').select("*").execute()
+drivers = pd.DataFrame(response_drivers.data)
+drivers = drivers.sort_values(by='driverId')
+
+st.table(drivers)
 
 
-url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
+st.image("https://www.formula1.com/etc/designs/fom-website/images/f1-tv-logo.svg", width=120) #https://www.formula1.com/etc/designs/fom-website/images/f1_logo.svg
+st.header('4ta temporada de la quiniela de Formula 1')
 
-conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+tab1, tab2 = st.tabs(["Resultados", "Pronosticos"])
+usuarios = pd.DataFrame({'Usuario': ['Seleccionar', 'Alex', 'Gerry', 'Giorgio', 'Mike']})
+usuario_activo = st.selectbox('Usuario', usuarios)
 
-data = conn.read(spreadsheet=url, usecols=[0, 1])
-st.dataframe(data)
+if usuario_activo is not 'Seleccionar':
+
+    # Consulta la base de datos para verificar los orderId existentes
+    response_pronosticos = supabase_client.table('Pronosticos').select("*").execute()
+    pronosticos = pd.DataFrame(response_pronosticos.data)
+    pronosticos = pronosticos.sort_values(by='id')
+    pronosticos = pronosticos[pronosticos['Usuario'] == usuario_activo]
+    pronosticos = pronosticos[pronosticos['Carrera No'] == 3]
 
 
-
-
-
-
-# import streamlit as st
-# from streamlit_gsheets import GSheetsConnection
-
-# conn = st.experimental_connection("gsheets", type=GSheetsConnection)
-
-# data = conn.read(worksheet="Summary")
-# st.dataframe(data)
+    st.dataframe(pronosticos, height=400)
