6dfc73cadc8d3375661e5bc50aa99b73971a7843
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-05-16 13:22:34 -0600

    Update quiniela2023.py

--- quiniela2023.py
+++ quiniela2023.py
@@ -54,6 +54,7 @@ hora_utc = datetime.datetime.now(pytz.utc)
 zona_mexico = pytz.timezone('America/Mexico_City')
 hora_mexico = hora_utc.astimezone(zona_mexico)
 
+pronosticos['hora_mexico'] = hora_mexico
 # pronostico_actual = pronosticos[pronosticos['Fecha Limite'] <= hora_mexico]
 
 
