924286bb769baf2973b0faed6f098fe785a1d0e0
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-05-16 12:13:57 -0600

    Update quiniela2023.py

--- quiniela2023.py
+++ quiniela2023.py
@@ -51,7 +51,7 @@ st.dataframe(pronosticos)
 
 if usuario_activo is not "Seleccionar":
     pronosticos = pronosticos[pronosticos['User'] == usuario_activo]
-    edited_pronosticos = st.data_editor(pronosticos, column_config={"Forecast": st.column_config.SelectboxColumn(options= drivers['Piloto'])}, disabled=["Race No", "Race", "Place", "Fecha", "Player", "Result"], hide_index=True)
+    edited_pronosticos = st.data_editor(pronosticos, column_config={"Forecast": st.column_config.SelectboxColumn(drivers['Piloto'])}, disabled=["Race No", "Race", "Place", "Fecha", "Player", "Result"], hide_index=True)
 # st.column_config.SelectboxColumn(label="Pronostico", *, width=None, help="Selecciona de la lista el piloto", width="medium", options=["Max", "Per"])
 
     # st.dataframe(edited_pronosticos)
