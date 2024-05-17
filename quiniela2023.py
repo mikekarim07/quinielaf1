b721f7244cfef8d90a9f480c5777d3c6db7a76c0
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-05-16 20:42:52 -0600

    Update quiniela2023.py

--- quiniela2023.py
+++ quiniela2023.py
@@ -34,7 +34,7 @@ users = conn.read(worksheet="Players", usecols=list(range(3)), ttl=5)
 
 
 if usuario_activo is not "Seleccionar":
-    clave_jugador = users.loc[players['user'] == usuario_activo, 'user_key'].values[0]
+    clave_jugador = users.loc[users['user'] == usuario_activo, 'user_key'].values[0]
     st.write(clave_jugador)
     
     # if pd.isna(clave_jugador):
