f983a51617c396cb10439cd438cb56cab1f8d323
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-05-23 17:24:18 -0600

    Update quiniela2023.py

--- quiniela2023.py
+++ quiniela2023.py
@@ -143,7 +143,7 @@ if usuario_activo != "Seleccionar":
         else:
             current_password = st.text_input("Ingresa tu Password", type="password")
             if current_password == user_pswd:
-                pronosticos = supabase_client.table('Pronosticos').select("id,Race,Place,User,Forecast").eq("User", usuario_activo).neq("Place", "Top 3").neq("Place", "Top 5").order('id', desc=False).execute()
+                pronosticos = supabase_client.table('Pronosticos').select("id,Race No,Race,Place,User,Forecast").eq("User", usuario_activo).neq("Place", "Top 3").neq("Place", "Top 5").order('id', desc=False).execute()
                 pronosticos = pd.DataFrame(pronosticos.data)
                 # pronosticos = pronosticos.sort_values(by='id')
                 pronosticos = pronosticos[(pronosticos['Race No'] == 10) | (pronosticos['Race No'] == 10)]
