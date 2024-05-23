df274c9ec5255a912356b9341e795bf05c263092
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-05-23 12:57:22 -0600

    Update quiniela2023.py

--- quiniela2023.py
+++ quiniela2023.py
@@ -116,7 +116,12 @@ drivers = drivers['driverName']
 # st.dataframe(pronosticos)
 
 users = supabase_client.table('users').select("*").eq("user", usuario_activo).execute()
-st.write(users.id)
+users = users.data
+# usuarios = obtener_usuarios()
+# usuario_nombres = {usuario['nombre']: usuario['id'] for usuario in usuarios}
+# selected_usuario_nombre = st.selectbox("Selecciona un usuario", list(usuario_nombres.keys()))
+# selected_usuario_id = usuario_nombres[selected_usuario_nombre]
+st.write(users)
 
 pronosticos = supabase_client.table('Pronosticos').select("*").eq("User", usuario_activo).execute()
 pronosticos = pd.DataFrame(pronosticos.data)
