1d9dbf081392ace8edf132ae3d25f7a5f288f751
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-06-06 17:44:32 -0600

    Update quiniela2023.py

--- quiniela2023.py
+++ quiniela2023.py
@@ -129,7 +129,7 @@ if usuario_activo != "Seleccionar":
 pronosticos_all['key1'] = pronosticos_all['Race'] + pronosticos_all['Place']
 st.dataframe(pronosticos_all)
 resultados_all['key1'] = resultados_all['Race'] + resultados_all['Place']
-pronosticos_all = pronosticos_all.merge(resultados_all, left_on="key_1", right_on='key_1', how='left')
+pronosticos_all = pronosticos_all.merge(resultados_all, left_on="key1", right_on='key1', how='left')
 
 st.dataframe(pronosticos_all)
 
